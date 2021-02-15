6e458cb68c2ac4cd1a5a66db26fc6ff019ff21cf
Eden Berhan Alem <edenberhanalem@gmail.com>
2021-02-16 01:10:48 +0300

    Draw the ball, two paddles and a line in the middle of the canvas as the middle line border

--- assets/js/app.js
+++ assets/js/app.js
@@ -55,3 +55,44 @@ function endGame(e) {
         worl.textContent = `${userScore} - ${compScore}: It's a tie! Try again`;
     }    
 }
+
+// Draw the ball
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "white";
+    ctx.fill();
+    ctx.closePath();
+}
+
+// Draw two paddles in which one is used by the user and the other by the computer
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "white";
+    ctx.fill();
+    ctx.font = "30px Arial";
+    ctx.fillText(compScore, 10, 50);
+    ctx.closePath();
+    
+}
+
+function drawPaddle2() {
+    ctx.beginPath();
+    ctx.rect(paddleX2, paddleHeight2 - 7, paddleWidth2, paddleHeight2);
+    ctx.fillStyle = "white";
+    ctx.fill();
+    ctx.font = "30px Arial";
+    ctx.fillText(userScore, 10, 250);
+    ctx.closePath();
+}
+
+// Draw a line in the middle of the canvas rectangle as a middle line border
+function drawLine() {
+    ctx = canvas.getContext('2d');
+    ctx.beginPath(); 
+    ctx.moveTo(0,canvas.height/2);
+    ctx.lineTo(canvas.width,canvas.height/2);
+    ctx.strokeStyle = "white";
+    ctx.stroke();
+}
